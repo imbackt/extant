3df5cebf3df9c7582854059499329dca2549d228
Ali AlRasheed <ali.alrasheed@outlook.com>
2021-06-29 22:47:03 +0300

    feat: implementing Window singleton

---
+++ src/main/java/com/imbackt/Main.java
@@ -0,0 +1,9 @@
+package com.imbackt;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.getInstance();
+        window.run();
+    }
+}
---
+++ src/main/java/com/imbackt/Window.java
@@ -0,0 +1,89 @@
+package com.imbackt;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Window {
+
+    private String title;
+    private int width;
+    private int height;
+    private long glfwWindow;
+
+    private static Window INSTANCE;
+
+    private Window() {
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Extant";
+    }
+
+    public static Window getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Window();
+        }
+        return INSTANCE;
+    }
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    private void init() {
+        // Setup an error callback
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW
+        if (!glfwInit()) {
+            throw new IllegalStateException("Unable to initialize GLFW!");
+        }
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        // Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create the GLFW window!");
+        }
+
+        // Make OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
+    }
+
+    private void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // Poll events
+            glfwPollEvents();
+
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
+}
